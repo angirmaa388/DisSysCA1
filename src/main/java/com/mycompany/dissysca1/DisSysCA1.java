6622192c071b2f3b40f32961fc3e75a76c1b1920
angirmaa388 <angirmaa388@gmail.com>
2025-07-08 22:34:56 +0100

    no message

---
+++ src/main/java/com/mycompany/dissysca1/DisSysCA1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dissysca1;
+
+/**
+ *
+ * @author angirmaa
+ */
+public class DisSysCA1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
